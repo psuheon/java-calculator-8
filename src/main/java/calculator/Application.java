1008ecce3cb4cb09f497333f305a9d5be5c6df71
psuheon <tony356527@gmail.com>
2025-10-16 16:01:33 +0900

    feat(Application): split string and caculate number Add new function to Application: - split the string entered by user to number list based on separator - add all splitted numbers

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,72 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
+
+    // 유저가 입력한 문자열을 구분자를 기준으로 하여 숫자들로 나누는 함수
+    public ArrayList<String> splitString(String str, char separator) {
+        ArrayList<String> separated = new ArrayList<String>();
+        int currentIndex = 0;
+        int numCount = 0;
+
+        if(separator == ' ') {
+            for(int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) == ',' || str.charAt(i) == ':') {
+                    separated.add(str.substring(currentIndex, i));
+                    numCount++;
+                    currentIndex = i + 1;
+                }
+            }
+        }
+        else {
+            currentIndex = 5;
+            for(int i = 5; i < str.length(); i++) {
+                if(str.charAt(i) == separator) {
+                    separated.add(str.substring(currentIndex, i));
+                    numCount++;
+                    currentIndex = i + 1;
+                }
+            }
+        }
+        separated.add(str.substring(currentIndex));
+
+        return separated;
+    }
+
+    // String 형식으로 된 숫자들을 Double 형식으로 바꾸어 더하는 함수
+    public Double calculate(ArrayList<String> nList) {
+        Double value = 0.0;
+        for(int i = 0; i < nList.size(); i++) {
+            String temp = nList.get(i);
+            value += Double.parseDouble(temp);
+        }
+
+        return value;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application calculator = new Application();
+
+        String userInput;
+        Double result;
+
+        userInput = Console.readLine();
+
+        if(userInput == "") {
+            result = 0.0;
+        }
+        else if(Character.isDigit(userInput.charAt(0))) {
+            ArrayList<String> numList = calculator.splitString(userInput, ' ');
+            result = calculator.calculate(numList);
+        }
+        else {
+            ArrayList<String> numList = calculator.splitString(userInput, userInput.charAt(2));
+            result = calculator.calculate(numList);
+        }
+
+        Console.close();
     }
 }
